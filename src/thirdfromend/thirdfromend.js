64b78948dbef154b56e761f42f5e2b27b914b3ed
Johnny <jkfaris@gmail.com>
2025-07-22 09:29:28 -0700

    solved thirffromend

--- src/thirdfromend/thirdfromend.js
+++ src/thirdfromend/thirdfromend.js
@@ -9,6 +9,33 @@ const LinkedList = require("../lib/linkedList");
  * null if list length < 3
  *
  */
-function thirdFromEnd(list) {}
+function thirdFromEnd(list) {
+  if (!list || !list.head) return null;
+
+  let pointer1 = list.head;
+  let i = 0;
+
+  // Move pointer1 ahead by 2 nodes
+  while (i < 2 && pointer1.next) {
+    pointer1 = pointer1.next;
+    i++;
+  }
+
+  // If the list has fewer than 3 nodes
+  if (i < 2) {
+    return null;
+  }
+
+  let pointer2 = list.head;
+
+  // Move both pointers until pointer1 reaches the end
+  while (pointer1.next) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
+
+  return pointer2;
+}
+
 
 module.exports = thirdFromEnd;
